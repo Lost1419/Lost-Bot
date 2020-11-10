8567ad514873feef4fefaa1ab8ca742737956e57
Lost <lostfrost1419@gmail.com>
2020-11-10 13:32:04 -0700

    Random Commands Added

--- Cogs/random.py
+++ Cogs/random.py
@@ -1,4 +1,6 @@
 import discord
+import requests
+import asyncio
 import typing
 from discord.ext import commands, tasks
 
@@ -11,6 +13,13 @@ def not_blacklisted():
             return False
         else:
             return True
+
+    return commands.check(predicate)
+
+
+def guild_owner():
+    def predicate(ctx):
+        return ctx.author.id == ctx.guild.owner_id
 
     return commands.check(predicate)
 
@@ -30,18 +39,9 @@ class Random(commands.Cog):
 
     @commands.group(invoke_without_command=True)
     @not_blacklisted()
-    async def spam(self, ctx, channel: discord.TextChannel,  member: typing.Optional[discord.Member], *, msg=None):
-        if member is not None:
-            self.spaming.start(channel=channel, spam_msg=f"<@!{member.id}>")
-            await ctx.send("Spam has been started!")
-        elif msg is not None:
-            self.spaming.start(channel=channel, spam_msg=msg)
-            await ctx.send("Spam has been started!")
-        elif msg and member is not None:
-            self.spaming.start(channel=channel, spam_msg=f"<@!{member.id}> {msg}")
-            await ctx.send("Spam has been started!")
-        else:
-            await ctx.send("That is unsupported")
+    async def spam(self, ctx, channel: discord.TextChannel, *, msg):
+        self.spaming.start(channel=channel, spam_msg=msg)
+        await ctx.send("Spam has been started!")
 
     @spam.command()
     @not_blacklisted()
@@ -52,6 +52,39 @@ class Random(commands.Cog):
     @tasks.loop(seconds=15)
     async def spaming(self, channel: discord.TextChannel, spam_msg: str):
         await channel.send(spam_msg)
+
+    @commands.command()
+    async def math(self, ctx, number_1: float, operator: str, number_2: float):
+        if operator == '*':
+            await ctx.send(f"{number_1}*{number_2}={number_1 * number_2}")
+        elif operator == '/':
+            await ctx.send(f"{number_1}/{number_2}={number_1 / number_2}")
+        elif operator == '+':
+            await ctx.send(f"{number_1}+{number_2}={number_1 + number_2}")
+        elif operator == '-':
+            await ctx.send(f"{number_1}-{number_2}={number_1 - number_2}")
+        else:
+            await ctx.send("Unsupported Type")
+
+    @commands.command()
+    async def type(self, ctx, channel: typing.Optional[discord.TextChannel], time: int, *, msg):
+        if channel is None:
+            channel = ctx.channel
+
+        async with channel.typing():
+            await asyncio.sleep(time)
+            await channel.send(msg)
+
+    @commands.command()
+    async def get(self, ctx, url: str):
+        request = requests.get(url)
+        print(request.text)
+
+    @commands.command()
+    @guild_owner()
+    async def test(self, ctx):
+        self.bot.load_extension("Cogs.test")
+        await ctx.send("Done!")
 
 
 def setup(bot):
